f39b67f2192e291c9ecc8b87170336e62bcc1618
fernandajrmn <fernandajrmn@gmail.com>
2025-09-15 14:33:41 -0600

    UART + Encoder

--- Encoder/Main.c
+++ Encoder/Main.c
@@ -11,6 +11,7 @@
 //PA3->Tx
 
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "stm32f051x8.h"
 #include <math.h>
@@ -22,6 +23,7 @@
 // Movement Variables
 float W;
 float Angle;
+char buffer[50];
 
 // Contador de interrupciones (opcional)
 volatile int count = 0;
@@ -36,6 +38,33 @@ volatile int8_t direction = 1;     // 1=CW, 0=CCW
 #define COUNTS_PER_REV 4000        // 1000 PPR × 4 (quadrature decoding)
 #define RPM_SAMPLE_TIME 100        // 100ms sampling period
 
+
+//------------------Serial------------------
+void USART2_Init(void) {
+    // 1. Activar el reloj para GPIOA y USART2
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
+    // 2. Configurar PA2 (TX) y PA3 (RX) como Alternate Function
+    GPIOA->MODER &= ~((3 << (2*2)) | (3 << (3*2)));   // limpiar
+    GPIOA->MODER |=  (2 << (2*2)) | (2 << (3*2));     // modo AF
+    GPIOA->AFR[0] |= (1 << (2*4)) | (1 << (3*4));     // AF1 = USART2
+
+    // 3. Configurar USART2
+    USART2->BRR = 8000000/9600;  // Baudrate = 9600, con fclk=48 MHz
+    USART2->CR1 = USART_CR1_TE | USART_CR1_UE; // Habilitar TX y USART
+}
+
+void USART2_SendChar(char c) {
+    while (!(USART2->ISR & USART_ISR_TXE)); // Esperar a que buffer esté vacío
+    USART2->TDR = c;
+}
+
+void USART2_SendString(const char *s) {
+    while (*s) {
+        USART2_SendChar(*s++);
+    }
+}
+
 // TIM3 – Encoder overflow interrupt
 void TIM3_IRQHandler(void)
 {
@@ -46,6 +75,9 @@ void TIM3_IRQHandler(void)
         total_ticks += direction ? 0x10000 : -0x10000;
     }
 }
+
+
+
 
 // TIM1 – RPM calculation every 100 ms
 void TIM1_BRK_UP_TRG_COM_IRQHandler(void)
@@ -76,9 +108,35 @@ void TIM1_BRK_UP_TRG_COM_IRQHandler(void)
         prev_ticks = current_ticks;
 
         // Angular velocity in rad/s
-        W = rpm * 2 * 3.14159f / 60.0f;
+       W = (int)rpm * 2 * 3.14159f / 60.0f;
+        //int w_frac = (int)((W) * 100); // 2 decimales
         Angle = (current_ticks * 360.0f)/COUNTS_PER_REV;
-        Angle = fmodf(Angle, 360.0f);
+        Angle = (int)fmodf(Angle, 360.0f);
+        //int angle_frac = (int)((Angle) * 100); // 2 decimales
+        int angle_int = (int)Angle;
+        int angle_frac = (int)((Angle - angle_int) * 100);
+
+        int w_int = (int)W;
+        int w_frac = (int)((W - w_int) * 100);
+
+
+
+        // Enviar valores por USART
+        //snprintf(buffer, sizeof(buffer), "Angle: %.2f deg\n", W);
+        //USART2_SendString(buffer);
+
+        // Convertir W y Angle a enteros para enviar por USART
+                //int angle_int = (int)Angle;
+                //int angle_frac = (int)((Angle - angle_int) * 100); // 2 decimales
+
+               // int w_int = (int)W;
+                //int w_frac = (int)((W - w_int) * 100); // 2 decimales
+
+        snprintf(buffer, sizeof(buffer), "Angle: %d.%02d deg | W: %d.%02d rad/s\n",
+        		angle_int, angle_frac, w_int, w_frac);
+
+        USART2_SendString(buffer);
+
     }
 }
 
@@ -119,38 +177,13 @@ void TIM6_IRQHandler(void)
    //float dir = direction ? 1.0f : -1.0f;
 }
 
-//------------------Serial------------------
-void USART2_Init(void) {
-    // 1. Activar el reloj para GPIOA y USART2
-    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
-
-    // 2. Configurar PA2 (TX) y PA3 (RX) como Alternate Function
-    GPIOA->MODER &= ~((3 << (2*2)) | (3 << (3*2)));   // limpiar
-    GPIOA->MODER |=  (2 << (2*2)) | (2 << (3*2));     // modo AF
-    GPIOA->AFR[0] |= (1 << (2*4)) | (1 << (3*4));     // AF1 = USART2
-
-    // 3. Configurar USART2
-    USART2->BRR = 8000000/9600;  // Baudrate = 9600, con fclk=48 MHz
-    USART2->CR1 = USART_CR1_TE | USART_CR1_UE; // Habilitar TX y USART
-}
-
-void USART2_SendChar(char c) {
-    while (!(USART2->ISR & USART_ISR_TXE)); // Esperar a que buffer esté vacío
-    USART2->TDR = c;
-}
-
-void USART2_SendString(const char *s) {
-    while (*s) {
-        USART2_SendChar(*s++);
-    }
-}
-
 
 
 int main(void)
 {
     configure_encoder();
     configure_rpm_timer();
+    USART2_Init();
     // Ahora angle_now contiene el ángulo instantáneo en grados
 
 
@@ -162,11 +195,16 @@ int main(void)
     TIM6->DIER |= (1 << 0);
     NVIC->ISER[0] |= (1 << 17);
 
-    USART2_Init();
+    //USART2_Init();
+
+    //char W_array[10];
+
 
     while (1) {
-        USART2_SendString("Hola\n");
-        for (volatile int i = 0; i < 500000; i++); // Delay burdo
+        //sprintf(W_array, "%.2f\n", W);  // actualizar cada ciclo
+        //USART2_SendString(W_array);
+        //for (volatile int i = 0; i < 500000; i++);
     }
+
 
 }
